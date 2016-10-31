51de7bd88f2edab248f1a1a658fb4b7c194a9fd9
Anas Halbawi <anashalb@gmail.com>
2016-10-31 09:18:55 +0400

    Error handling

--- bin/font-to-swift.js
+++ bin/font-to-swift.js
@@ -3,54 +3,47 @@
 (function() {
 
 	var path = require('path');
-	var cssToSwift = require('../lib/fontToSwift');
+	var fontToSwift = require('../lib/fontToSwift');
 	var fs = require('fs');
 	var args = require('minimist')(process.argv.slice(2));
+	var promise = require('promise');
 
-	function getPath(inPath) {
-
-		if (inPath.startsWith("http")) return inPath;
-
-		if (inPath.charAt(0) != '/') {
-
-			var directory = ".";
-			return path.normalize(path.join(directory, inPath));
-		} else {
-			return inPath;
-		}
-	}
-
-
+	//Only argument necessary for running is either a path to a file, a directory or a URL to download from
 	if (!args['_'][0]) {
 
 		console.log("Error: Please specify an input file name or a directory.");
 	} else {
 
-		var inPath = getPath(args['_'][0]);
-		var outPath = args['_'][1]
+		var inPath = args['_'][0];	//File, directory or url
+		var outPath = args['_'][1];	//Must be a directory
 
-		if (outPath) {
+
+		if (outPath) {	//Validate the output directory
 
 			if (!fs.existsSync(outPath) || !fs.lstatSync(outPath).isDirectory()) {
 				console.log("Error: The output " + outPath + " is not a valid directory.");
 				return;
 			}
-		} else {
+		} else {	//No directory: set the output directory to the input directory, or to the current directory if a URL is passed
 
 			if (fs.existsSync(inPath)) {
 
-				if (fs.lstatSync(inPath).isDirectory()) {
+				if (fs.lstatSync(inPath).isDirectory()) {	//Input is a directory
 					outPath = inPath;
 				} else {
-					outPath = path.dirname(inPath);
+					outPath = path.dirname(inPath);	//Input is a file
 				}
-			} else {
+			} else {	//URL
 
 				outPath = ".";
 			}
 		}
 
-		cssToSwift(inPath, outPath);
-	}
+		fontToSwift(inPath, outPath).then(function() {
 
+		}, function(e){
+
+			console.log(e);
+		});
+	}
 }());
